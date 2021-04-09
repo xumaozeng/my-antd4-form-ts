d91ced5aca7b2d9130af482b68b571ca55166125
xumaozeng <15611895341@163.com>
2021-04-09 18:00:11 +0800

    项目初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,5 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h3>react typescript antd4 form</h3>;
 }
 
 export default App;
